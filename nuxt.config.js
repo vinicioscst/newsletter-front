480ec66056a24ee74fc81f1ec98e7148d341cfd4
Vinícios Costa <dev.vinicios@gmail.com>
2024-05-31 22:50:21 -0300

    fix: add api base url to environment variable

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://newsapi.org/v2/',
+    baseURL: process.env.BASE_URL,
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
@@ -68,6 +68,6 @@ export default {
   build: {},
 
   env: {
-    APIKey: process.env.API_KEY || '',
+    APIKey: process.env.BASEURL || '',
   },
 }
