b84b1d551c97727e4111064d483f08eec7584536
Vinícios Costa <dev.vinicios@gmail.com>
2024-06-02 23:05:29 -0300

    feat: create index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>Hello, world!</h1>
+  <div>
+    <v-app-bar
+      extended
+      dark
+      elevation="0"
+      class="d-flex align-center justify-center gap-4 px-6 blue-grey darken-4"
+    >
+      <v-icon large class="pr-4"> mdi-newspaper </v-icon>
+      <h1>Newsletter</h1>
+    </v-app-bar>
+    <v-main class="py-14">
+      <ArticleList></ArticleList>
+    </v-main>
+  </div>
 </template>
 
 <script>
