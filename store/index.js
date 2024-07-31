f2c5e59a2010d1b988e814c0807b09e23eacd689
Vinícios Costa <dev.vinicios@gmail.com>
2024-07-31 15:50:31 -0300

    feat: added articles store

---
+++ store/index.js
@@ -0,0 +1,34 @@
+export const state = () => ({
+  articles: {},
+})
+
+export const getters = {
+  getArticles(state) {
+    return state.articles.articles
+  },
+  getTotalPages(state) {
+    return Math.ceil(state.articles.count / 12)
+  },
+}
+
+export const mutations = {
+  setArticles(state, articles) {
+    state.articles = articles
+  },
+}
+
+export const actions = {
+  async fetchArticles({ commit }) {
+    try {
+      const response = await this.$axios.$get(`/api/articles?perPage=12`)
+      commit('setArticles', response)
+      return response
+    } catch (error) {
+      console.error('Error fetching articles:', error)
+    }
+  },
+
+  async nuxtServerInit({ dispatch }) {
+    await dispatch('fetchArticles')
+  },
+}
