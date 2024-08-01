92a92df9a7e63de152100f8dd20cbe5fcf4ba7bb
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-01 11:49:36 -0300

    feat: add pagination logic

---
+++ components/Pagination.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-pagination
+    v-model="currentPage"
+    :length="totalPages"
+    @input="handlePageChange"
+    color="orange darken-3"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentPage: 1,
+      totalPages: this.$store.getters['getTotalPages'],
+    }
+  },
+
+  methods: {
+    async handlePageChange(page) {
+      this.currentPage = page
+      await this.$store.dispatch('fetchArticles', page)
+    },
+  },
+}
+</script>
