ca2c8dace35f6ed67e9f64732f0e063acf8c1cc3
Vinícios Costa <dev.vinicios@gmail.com>
2024-07-31 21:55:27 -0300

    feat: add component to list articles topics

---
+++ components/TopicCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="`rounded-xl white--text px-3 py-1 ${color} text-no-wrap`">
+    <p class="text-lowercase mb-0">{{ `#${subject}` }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    subject: {
+      type: String,
+      default: () => '',
+    },
+    color: {
+      type: String,
+      default: () => '',
+    },
+  },
+}
+</script>
---
+++ components/TopicsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2 class="pb-6">Tópicos</h2>
+    <div
+      class="grey lighten-4 px-4 py-3 rounded-xl d-flex gap-4 overflow-x-auto"
+    >
+      <TopicCard
+        v-for="subject in subjects"
+        :key="subject"
+        :subject="subject"
+        :color="generateColor(colors.length)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subjects: this.$store.getters['getTopics'],
+      colors: [
+        'red darken-3',
+        'pink darken-3',
+        'purple darken-3',
+        'deep-purple darken-3',
+        'indigo darken-3',
+        'deep-orange darken-3',
+      ],
+    }
+  },
+
+  methods: {
+    generateColor(arrayLength) {
+      return this.colors[Math.trunc(Math.random() * arrayLength)]
+    },
+  },
+}
+</script>
