ce48ca39e978832720611a91c62b812d1c452570
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-01 11:49:11 -0300

    fix: adjusts articles list and cards

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -6,25 +6,27 @@
     elevation="0"
     class="mb-3 blue-grey lighten-5"
   >
-    <v-img v-bind="articleImg">
+    <v-img v-bind="articleImg" class="article-img">
+      <v-chip :class="`ml-4 ${color} white--text`">{{
+        articleData?.subtopic || 'Subtópico indisponível'
+      }}</v-chip>
       <v-card-title tag="h3" class="text-break">
-        {{ articleData.title }}
+        {{ articleData?.title || 'Título Indisponível' }}
       </v-card-title>
       <v-card-subtitle class="grey--text lighten-5">{{
-        articleData.source
+        articleData?.source || 'Fonte Indisponível'
       }}</v-card-subtitle>
+      <div class="article-img-gradient"></div>
     </v-img>
-
-    <v-card-subtitle>{{ articleData.headline }}</v-card-subtitle>
     <div class="d-flex flex-row align-center justify-space-between px-4 py-4">
       <v-card-text
-        class="px-0 py-0 blue-grey--text darken-4"
+        class="px-0 py-0 grey--text text--darken-1"
         :title="formattedDateTitle"
       >
         {{ formattedDate }}
       </v-card-text>
-      <a :href="articleData.url" target="_blank">
-        <v-btn small class="blue-grey lighten-1 white--text"> Leia mais </v-btn>
+      <a :href="articleData?.url || '#'" target="_blank">
+        <v-btn small outlined color="grey darken-1"> Leia mais </v-btn>
       </a>
     </div>
   </v-card>
@@ -37,19 +39,25 @@ export default {
       type: Object,
       default: () => {},
     },
+    color: {
+      type: String,
+      default: () => '',
+    },
   },
   data() {
     return {
       articleData: this.article,
       articleImg: {
         height: '300px',
-        src: `https://via.placeholder.com/600x400/263238?text=${this.article.topic}`,
+        src: this.article.image,
         class: 'white--text align-end',
       },
     }
   },
   computed: {
     formattedDate() {
+      if (!this.articleData.publishedAt) return ''
+
       const articleDate = new Date(this.articleData.publishedAt)
       const today = new Date()
       const millisecondsDiff = today.getTime() - articleDate.getTime()
@@ -61,6 +69,8 @@ export default {
       )
     },
     formattedDateTitle() {
+      if (!this.articleData.publishedAt) return ''
+
       const articleDate = new Date(this.articleData.publishedAt)
 
       return new Intl.DateTimeFormat('pt-BR').format(articleDate)
@@ -72,5 +82,18 @@ export default {
 <style lang="scss">
 .text-break {
   word-break: break-all;
+}
+
+.article-img {
+  position: relative;
+}
+
+.article-img-gradient {
+  position: absolute;
+  top: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(0deg, #141414 0%, #ffffff00 100%);
 }
 </style>
--- components/ArticleList.vue
+++ components/ArticleList.vue
@@ -1,23 +1,9 @@
 <template>
-  <v-responsive width="100%" max-width="1000px" class="px-4 mx-auto">
-    <div
-      width="100%"
-      class="d-flex flex-row align-center justify-space-between pb-6"
-    >
-      <h2 class="pr-2">Últimas notícias</h2>
-      <a
-        href="https://news.google.com/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNREUxWm5JU0JYQjBMVUpTS0FBUAE?hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
-        target="_blank"
-        ><v-btn class="blue-grey darken-1 white--text"
-          >Todas as notícias</v-btn
-        ></a
-      >
-    </div>
+  <v-responsive width="100%" class="mx-auto">
     <v-list
-      v-if="articles.length === 0"
+      v-if="isLoading"
       tag="ul"
       width="100%"
-      max-width="1000px"
       class="transparent py-0 px-0 mx-auto grid-2col"
     >
       <v-skeleton-loader type="card" class="v-flex flex-column gap-4" />
@@ -26,17 +12,25 @@
       <v-skeleton-loader type="card" class="v-flex flex-column gap-4" />
     </v-list>
     <v-list
-      v-if="articles.length !== 0"
+      v-if="hasArticles"
       tag="ul"
       width="100%"
-      max-width="1000px"
       class="transparent py-0 px-0 mx-auto grid-2col"
     >
       <ArticleCard
         v-for="article in articles"
-        :key="article.url"
+        :key="article.title"
         :article="article"
+        :color="generateColor(colors.length)"
       />
+    </v-list>
+    <v-list
+      v-if="!hasArticles"
+      tag="ul"
+      width="100%"
+      class="transparent py-0 px-0 mx-auto grid-2col"
+    >
+      <h3 class="text-center pb-10">Nenhuma notícia encontrada</h3>
     </v-list>
   </v-responsive>
 </template>
@@ -45,33 +39,40 @@
 export default {
   data() {
     return {
-      articles: [],
+      colors: [
+        'red darken-3',
+        'pink darken-3',
+        'purple darken-3',
+        'deep-purple darken-3',
+        'indigo darken-3',
+        'deep-orange darken-3',
+      ],
     }
   },
-
-  async beforeMount() {
-    await this.getArticles()
+  computed: {
+    articles() {
+      return this.$store.getters['getArticles']
+    },
+    hasArticles() {
+      return this.articles.length > 0
+    },
+    isLoading() {
+      return this.$store.getters['getLoadingStatus']
+    },
   },
-
   methods: {
-    async getArticles() {
-      this.articles = await this.$axios.$get(`/api/articles`)
+    generateColor(arrayLength) {
+      return this.colors[Math.trunc(Math.random() * arrayLength)]
     },
   },
 }
 </script>
 
 <style lang="scss">
-$screen-sm-min: 640px;
-
 .grid-2col {
-  display: grid !important;
-  grid-template-columns: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
   gap: 1.5rem;
-
-  @media (min-width: $screen-sm-min) {
-    grid-template-columns: auto auto;
-  }
 }
 
 .gap-4 {
