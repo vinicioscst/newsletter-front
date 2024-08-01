9d3573db6f7e31ff1b4fc8d3882375fbbf4dac2d
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-01 11:51:56 -0300

    feat: create search component

---
+++ components/SearchArticles.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-form>
+    <v-text-field
+      append-icon="mdi-magnify"
+      color="orange darken-3"
+      placeholder="Pesquise pelo título"
+      filled
+      rounded
+    />
+  </v-form>
+</template>
