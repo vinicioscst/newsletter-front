c2b8db3d7462e9eb32eeaf5d10c72df3c0732704
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-05 15:42:10 -0300

    chore: add cookie lib

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'cookie-universal-nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
