d3f5772641801ff7eb1b78841122304d7a1fec19
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-07 13:30:38 -0300

    feat: adds dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <DashboardHeader />
+    <div class="page">
+      <div class="page-container">
+        <Nuxt />
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DashboardLayout',
+}
+</script>
+
+<style scoped>
+* {
+  font-family: 'Inter', sans-serif;
+}
+
+.dashboard-container {
+  display: grid;
+  grid-template-columns: auto 1fr;
+}
+
+.page {
+  padding: 0 16px;
+  width: 100%;
+}
+
+.page-container {
+  max-width: 1280px;
+  margin: 0 auto;
+}
+</style>
