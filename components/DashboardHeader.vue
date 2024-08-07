9572e6cb623a53e83faa2e139ce4a1987e7ba500
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-07 13:30:17 -0300

    feat: creates dashboard menu components

---
+++ components/DashboardHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="py-11 mb-11 grey darken-4 rounded-b-xl">
+      <v-app-bar elevation="0" class="transparent">
+        <div class="header-container justify-space-between">
+          <NuxtLink to="/admin">
+            <h1 class="logo">Dashboard</h1>
+          </NuxtLink>
+          <DashboardUserOptions />
+        </div>
+      </v-app-bar>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-container {
+  width: 100%;
+  max-width: 1280px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  position: relative;
+}
+
+.header-container a {
+  text-decoration: none;
+}
+
+.logo {
+  color: white;
+  text-transform: lowercase;
+  user-select: none;
+}
+</style>
---
+++ components/DashboardUserOptions.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-menu bottom left min-width="200px" rounded offset-y>
+    <template v-slot:activator="{ on }">
+      <v-btn icon large v-on="on">
+        <v-avatar color="grey lighten-3">
+          <v-icon> mdi-account-circle </v-icon>
+        </v-avatar>
+      </v-btn>
+    </template>
+
+    <v-card class="mt-2">
+      <v-list-item-content class="pa-0">
+        <div class="mx-auto text-center">
+          <div class="user-info">
+            <h3>{{ user?.name }}</h3>
+            <p>
+              {{ user?.email }}
+            </p>
+          </div>
+          <v-divider></v-divider>
+          <v-btn
+            nuxt
+            to="/admin/settings"
+            color="transparent"
+            elevation="0"
+            class="menu-options py-5 rounded-0"
+          >
+            Configurações
+          </v-btn>
+          <v-divider></v-divider>
+          <v-btn
+            @click="handleLogout"
+            color="orange darken-4"
+            elevation="0"
+            class="menu-options py-5 white--text rounded-0"
+          >
+            Logout
+          </v-btn>
+        </div>
+      </v-list-item-content>
+    </v-card>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.getters['user/getUser']
+    },
+  },
+  methods: {
+    handleLogout() {
+      this.$store.dispatch('user/logoutUser', {
+        cookies: this.$cookies,
+        toast: this.$toast,
+        router: this.$router,
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.user-info {
+  user-select: none;
+  padding: 1rem;
+}
+.user-info p {
+  margin: 0;
+}
+.menu-options {
+  width: 100%;
+}
+</style>
