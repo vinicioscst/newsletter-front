bd145aa536aecae9792ed83dbdd08c82690c8cc5
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-08 15:22:20 -0300

    feat: loads user info on settings page

--- pages/admin/settings.vue
+++ pages/admin/settings.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Settings</h1>
+  <div>
+    <h1>Settings</h1>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'AdminSettingsPage',
+  layout: 'dashboard',
+  computed: {
+    user() {
+      return this.$store.getters['user/getUser']
+    },
+  },
+
+  async beforeMount() {
+    if (this.user === null) {
+      try {
+        await this.$store.dispatch('user/loadUser')
+      } catch (error) {}
+    }
+  },
 }
 </script>
