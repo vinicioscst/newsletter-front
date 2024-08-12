d9064425a753ef2206caa9e000f750cfc0940929
Vinícios Costa <dev.vinicios@gmail.com>
2024-08-11 21:59:00 -0300

    fix: removes scss file from nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,7 +77,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: [],
     theme: {
       dark: false,
       themes: {
