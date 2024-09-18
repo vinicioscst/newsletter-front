ab1266004f23b7f7ed910389647ad66d136872dd
Vinícios Costa <dev.vinicios@gmail.com>
2024-09-18 03:22:48 -0300

    test: separates tests into different files

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -30,13 +30,21 @@ describe('home page', () => {
     it('renders article modal on click', () => {
       cy.get('[data-test=article-preview-modal]').should('not.exist')
       cy.get('[data-test=card-button]').first().click()
-      cy.get('[data-test=article-preview-modal]').should('exist')
-      cy.get('[data-test=article-preview-modal]').should('be.visible')
+      cy.get('[data-test=article-preview-modal]')
+        .should('exist')
+        .and('be.visible')
     })
   })
 
-  context('filter and ordening', () => {
-    //
+  context('search, filter and ordening', () => {
+    it('search 1/3 - shows all articles when searching without text')
+    it(
+      'search 2/3 - shows no article message when searching for inexistent article or topic'
+    )
+    it('search 3/3 - shows correct article(s) when searching for it')
+    it('order 1/2 - shows articles in alphabetical order')
+    it('order 2/2 - orders articles by date')
+    it('filter - show correct amount of articles')
   })
 
   context('navigation', () => {
@@ -51,43 +59,6 @@ describe('home page', () => {
       cy.get('[data-test=header-login-button-mobile]').should('be.visible')
       cy.get('[data-test=header-login-button-mobile]').click()
       cy.location('pathname').should('include', 'login')
-    })
-  })
-})
-
-describe('login page', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:8000/login')
-  })
-
-  context('validation', () => {
-    it('verifies if form has empty field(s) before submitting 1/3', () => {
-      cy.get('div .error--text').should('not.exist')
-      cy.get('[data-test=login-form-button]').click()
-      cy.get('div .error--text').should('exist')
-      cy.get('[data-test=login-form-button]').should('be.disabled')
-    })
-
-    it('verifies if form has empty field(s) before submitting 2/3', () => {
-      cy.get('[data-test=login-form-email]').type('meuemail@ht.com')
-      cy.get('div .error--text').should('not.exist')
-      cy.get('[data-test=login-form-button]').click()
-      cy.get('div .error--text').should('exist')
-      cy.get('[data-test=login-form-button]').should('be.disabled')
-    })
-
-    it('verifies if form has empty field(s) before submitting 3/3', () => {
-      cy.get('[data-test=login-form-password]').type('minhasenha')
-      cy.get('div .error--text').should('not.exist')
-      cy.get('[data-test=login-form-button]').click()
-      cy.get('div .error--text').should('exist')
-      cy.get('[data-test=login-form-button]').should('be.disabled')
-    })
-
-    it('verifies if form has invalid value', () => {
-      cy.get('[data-test=login-form-email]').type('meuemail@com')
-      cy.get('div .error--text').should('exist')
-      cy.get('[data-test=login-form-button]').should('be.disabled')
     })
   })
 })
